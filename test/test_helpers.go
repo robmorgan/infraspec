9b0595ea855fdcb8369581c7051803dacc4a4389
Rob Morgan <robbym@gmail.com>
2025-03-07 11:38:36 +0800

    expand test helpers

--- test/test_helpers.go
+++ test/test_helpers.go
@@ -30,16 +30,25 @@ func GetTestConfig(t *testing.T) *config.Config {
 	return cfg
 }
 
+func configureEnvForTests() {
+	os.Setenv("AWS_ACCESS_KEY_ID", "test")
+	os.Setenv("AWS_SECRET_ACCESS_KEY", "test")
+	os.Setenv("AWS_DEFAULT_REGION", "us-east-1")
+	os.Setenv("AWS_ENDPOINT_URL", "http://localhost:4566")
+}
+
 func writeLocalStackProviderConfig(terraformDir string) error {
 	providerContent := `
 provider "aws" {
+  region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
-  region                      = "us-east-1"
+
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+  skip_region_validation      = true
   skip_requesting_account_id  = true
-  
+
   endpoints {
     dynamodb = "http://localhost:4566"
     s3       = "http://localhost:4566"
